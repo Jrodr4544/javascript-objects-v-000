968aa00ce359212cbf49bb81e3e4a611d93c7384
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2018-06-27 20:42:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {'Joey Badass':'Daily Routine'}
 var updatePlaylist = (playlist, artistName, songTitle) => {
   playlist[artistName] = songTitle
 }
+
+var removeFromPlaylist = (playlist, artistName) => {
+  
+}
